a10c725e4dd564038ce09c3299b3975b532c5192
daniel leahy <daniel.leahy@ucd.ie>
2023-05-14 13:58:07 +0100

    append and remove last methods added

--- src/datastructions/linkedlist/LinkedList.java
+++ src/datastructions/linkedlist/LinkedList.java
@@ -13,12 +13,73 @@ public class LinkedList {
         length = 1L;
     }
 
+    public Node removeLast(){
+        if(length == 0) return  null;
+
+        Node pre = head;
+        Node temp = head;
+
+        while(temp.next != null){
+            pre = temp;
+            temp = temp.next;
+        }
+
+       tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return  temp;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+
+        } else {
+           tail.next = newNode;
+           tail = newNode;
+        }
+        length++;
+    }
+
+
     public void printList() {
         Node temp = head;
         while (temp != null) {
             System.out.println(temp.value);
             temp = temp.next;
         }
+    }
+
+    public Node getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public void getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
     }
 
     class Node {
@@ -48,26 +109,5 @@ public class LinkedList {
 
     }
 
-    public Node getHead() {
-        if (head == null){
-            System.out.println("Head: null");
-        } else {
-            System.out.println("Head: " + head.value);
-        }
-        return head;
-    }
-    public void setHead(Node head) {
-        this.head = head;
-    }
-    public void getTail() {
-        if (head == null) {
-            System.out.println("Tail: null");
-        } else {
-            System.out.println("Tail: " + tail.value);
-        }
-    }
 
-    public void getLength() {
-        System.out.println("Length: " + length);
-    }
 }
--- src/datastructions/linkedlist/Main.java
+++ src/datastructions/linkedlist/Main.java
@@ -6,26 +6,18 @@ public class Main {
     public static void main(String[] args) {
 
         LinkedList myLinkedList = new LinkedList(4);
+        myLinkedList.append(10);
+        myLinkedList.printList();
 
+        myLinkedList.removeLast();
+        printHeadTail(myLinkedList);
+
+    }
+
+    private static void printHeadTail(LinkedList myLinkedList) {
         myLinkedList.getHead();
         myLinkedList.getTail();
         myLinkedList.getLength();
-
-        System.out.println("\nLinked List:");
-        myLinkedList.printList();
-
-        /*
-            EXPECTED OUTPUT:
-            ----------------
-            Head: 4
-            Tail: 4
-            Length: 1
-
-            Linked List:
-            4
-
-        */
-
     }
 
 }
