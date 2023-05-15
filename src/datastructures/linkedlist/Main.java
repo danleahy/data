2bad56fd4e57c5230c7525802c223a4e05ffe7e8
danleahy <danleahy@gmail.com>
2023-05-15 21:35:48 +0100

    adding more methods to linked list
    remove first
    prepend
    get

--- src/datastructures/linkedlist/Main.java
+++ src/datastructures/linkedlist/Main.java
@@ -5,13 +5,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(4);
-        myLinkedList.append(10);
-        myLinkedList.printList();
+        LinkedList myLinkedList = new LinkedList(2);
+        myLinkedList.append(1);
 
-        myLinkedList.removeLast();
-        printHeadTail(myLinkedList);
+        // (2) Items - Returns 2 Node
+        System.out.println(myLinkedList.removeFirst().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myLinkedList.removeFirst().value);
+        // (0) Items - Returns null
+        System.out.println(myLinkedList.removeFirst());
 
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            2
+            1
+            null
+
+        */
     }
 
     private static void printHeadTail(LinkedList myLinkedList) {
