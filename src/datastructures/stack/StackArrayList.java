cf2a783bebabc2c0afa6404a3b96c4df707fc730
danleahy <danleahy@gmail.com>
2023-08-09 21:20:19 +0100

    added stack implementation using Array List

---
+++ src/datastructures/stack/StackArrayList.java
@@ -0,0 +1,43 @@
+package datastructures.stack;
+
+import java.util.ArrayList;
+
+public class StackArrayList<T> {
+
+    private final ArrayList<T> stackList = new ArrayList<>();
+
+    public ArrayList<T> getStackList() {
+        return stackList;
+    }
+
+    public void printStack() {
+        for (int i = stackList.size()-1; i >= 0; i--) {
+            System.out.println(stackList.get(i));
+        }
+    }
+
+    public void printStack2() {
+        stackList.forEach(System.out::println);
+    }
+
+    public boolean isEmpty() {
+        return stackList.isEmpty();
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            return stackList.get(stackList.size() - 1);
+        }
+    }
+
+    public int size() {
+        return stackList.size();
+    }
+
+    public void push(T value) {
+        stackList.add(value);
+    }
+
+}
