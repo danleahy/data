4b64cd1bc45397d41bec6b71fa257f7626f393c4
danleahy <danleahy@gmail.com>
2023-10-29 16:56:08 +0000

    sinkdown method added used for fixing the right location within the
    bst contains method updated to use recursion
    
    Factorial class added

--- src/datastructures/tree/Main.java
+++ src/datastructures/tree/Main.java
@@ -5,7 +5,20 @@ public class Main {
     public static void main(String[] args) {
         BinarySearchTree binarySearchTree = new BinarySearchTree();
 
-        System.out.println(binarySearchTree.root);
+        binarySearchTree.insert(47);
+        binarySearchTree.insert(21);
+        binarySearchTree.insert(76);
+        binarySearchTree.insert(18);
+        binarySearchTree.insert(27);
+        binarySearchTree.insert(52);
+        binarySearchTree.insert(82);
+
+//        System.out.println(binarySearchTree.root);
+
+        System.out.println(binarySearchTree.contains(83));
+        System.out.println(binarySearchTree.rContains(27));
+
+
 
     }
 }
