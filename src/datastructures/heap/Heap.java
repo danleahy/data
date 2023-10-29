c49730a1e48952898b62fc7079d3b8cece362b0e
danleahy <danleahy@gmail.com>
2023-10-29 16:55:24 +0000

    sinkdown method added used for fixing the right location within the
    bst contains method updated to use recursion

--- src/datastructures/heap/Heap.java
+++ src/datastructures/heap/Heap.java
@@ -15,16 +15,51 @@ public class Heap {
         return new ArrayList<>(heap);
     }
 
+
+    private void sinkDown(int index) {
+        int maxIndex = index;
+        while (true) {
+            int leftIndex = leftChild(index);
+            int rightIndex = rightChild(index);
+
+            if (leftIndex < heap.size() && heap.get(leftIndex) > heap.get(maxIndex)) {
+                maxIndex = leftIndex;
+            }
+
+            if (rightIndex < heap.size() && heap.get(rightIndex) > heap.get(maxIndex)) {
+                maxIndex = rightIndex;
+            }
+
+            if (maxIndex != index) {
+                swap(index, maxIndex);
+                index = maxIndex;
+            } else {
+                return;
+            }
+        }
+    }
+
     public  void insert(int value){
         heap.add(value);
         int current = heap.size() - 1;
 
-        while (current > 0){
+        while (current > 0 && heap.get(current) > heap.get(parent(current))){
             swap(current,parent(current));
             current = parent(current);
         }
-
     }
+
+
+    //insert for min Heap
+//    public void insert(int value) {
+//        heap.add(value);
+//        int current = heap.size() - 1;
+//
+//        while (current > 0 && heap.get(current) < heap.get(parent(current))) {
+//            swap(current, parent(current));
+//            current = parent(current);
+//        }
+//    }
 
 
     private int leftChild(int index){
@@ -32,7 +67,7 @@ public class Heap {
     }
 
     private int rightChild(int index){
-        return 2 * index + 1 ;
+        return 2 * index + 2 ;
     }
 
 
@@ -44,5 +79,20 @@ public class Heap {
         int temp = heap.get(index1);
         heap.set(index1,heap.get(index2));
         heap.set(index2,temp);
+    }
+
+    public Integer remove(){
+        if (heap.isEmpty()) return null;
+
+
+        if (heap.size() == 1) {
+            return heap.remove(0);
+        }
+
+        int maxValue = heap.get(0);
+        heap.set(0,heap.remove(heap.size() - 1));
+        sinkDown(0);
+
+        return maxValue;
     }
 }
