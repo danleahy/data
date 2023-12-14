dfb87a0a960d24a379c36b126880914b094edd4c
danleahy <danleahy@gmail.com>
2023-12-14 15:48:36 +0000

    bubble sort completed selection sort completed
    refactoring of packages to match udemy's instructors package management

---
+++ src/algorithms/sorts/SelectionSort/SelectionSortTest.java
@@ -0,0 +1,25 @@
+package algorithms.sorts.SelectionSort;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SelectionSortTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void selectionSort() {
+        //Arrange
+        int[] array = {6,4,7,3,2,1};
+
+        int[] expected = {1,2,3,4,6,7};
+        //Act
+        SelectionSort.selectionSort(array);
+        //Assert
+        assertArrayEquals(expected,array);
+    }
+}
