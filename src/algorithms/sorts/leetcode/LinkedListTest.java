7309f37c22bd9c2c937f13160a9af037ee3a153d
danleahy <danleahy@gmail.com>
2023-12-16 21:21:25 +0000

    bubble sort completed selection sort completed
    linked list leet code sorting algorithm questions
    
    way more difficult than array based

---
+++ src/algorithms/sorts/leetcode/LinkedListTest.java
@@ -0,0 +1,49 @@
+package algorithms.sorts.leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    private LinkedList linkedList;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void bubbleSort() {
+        //Arrange
+        linkedList = new LinkedList(4);
+        linkedList.append(3);
+        linkedList.append(2);
+        linkedList.append(1);
+        linkedList.append(5);
+        linkedList.append(6);
+        linkedList.append(7);
+        //Act
+        linkedList.bubbleSort();
+        //Assert
+
+        linkedList.printList();
+    }
+
+    @Test
+    void selectionSort() {
+        //Arrange
+        linkedList = new LinkedList(4);
+//        linkedList.append(3);
+//        linkedList.append(2);
+//        linkedList.append(1);
+        linkedList.append(8);
+        linkedList.append(7);
+        linkedList.append(6);
+        //Act
+        linkedList.selectionSort();
+        //Assert
+
+        linkedList.printList();
+    }
+}
