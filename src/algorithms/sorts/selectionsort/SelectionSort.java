9356bae2213a724aed9e182fe0255d630afd03e6
danleahy <danleahy@gmail.com>
2024-04-16 14:06:56 +0100

    refactored to make it clearer

--- src/algorithms/sorts/selectionsort/SelectionSort.java
+++ src/algorithms/sorts/selectionsort/SelectionSort.java
@@ -6,23 +6,29 @@ public class SelectionSort {
 
     public static void selectionSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
-            int minIndex = i;
-
-            for (int j = i+1; j < array.length; j++) {
-                if (array[j] < array[minIndex]) {
-                    minIndex = j;
-                }
-            }
+            int minIndex = findSmallest(array, i);
             if (i != minIndex) {
-                //swap
-                int temp = array[i];
-                array[i] = array[minIndex];
-                array[minIndex] = temp;
+                swap(array, i, minIndex);
             }
         }
     }
 
+    private static int findSmallest(int[] array, int i) {
+        int minIndex = i;
 
+        for (int j = i + 1; j < array.length; j++) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+        }
+        return minIndex;
+    }
+
+    private static void swap(int[] array, int i, int minIndex) {
+        int temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
+    }
 
 
     public static void main(String[] args) {
@@ -30,6 +36,7 @@ public class SelectionSort {
         int[] array  = {30,45,1,3,90};
 
         selectionSort(array);
+
         System.out.println(Arrays.toString(array));
     }
 }
